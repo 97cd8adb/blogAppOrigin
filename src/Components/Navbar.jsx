cb4570e266dcf66e3a413d57f17b91fb0b21878d
1sh1d4 <isd97cd8adb@icloud.com>
2025-08-08 18:50:42 +0900

    各ページの要素を追加

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
-const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+const Navbar = ( {isLoggedIn} ) => {
+      return (
+        <nav>
+          <ul>
+            <li><Link to="/">ホーム</Link></li>
+            {isLoggedIn ? (
+              <>
+                <li><Link to="/create">記事投稿</Link></li>
+                <li><Link to="/logout">ログアウト</Link></li>
+              </>
+            ) : (
+              <>
+                <li><Link to="/login">ログイン</Link></li>
+              </>
+            )}
+          </ul>
+        </nav>
+      )
+    }
 
 export default Navbar

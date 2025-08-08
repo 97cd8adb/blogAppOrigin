24fa93cfc3e6e6631b94bef4857b576e2f09852b
1sh1d4 <isd97cd8adb@icloud.com>
2025-08-08 21:00:03 +0900

    ポスト作成機能実装開始

--- src/Components/CreatePost.jsx
+++ src/Components/CreatePost.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { addDoc, collection, getFirestore,  } from 'firebase/firestore';
 
 const CreatePost = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] =useState("");
+  const db = getFirestore();
+  const postsCollectionRef = collection(db, 'posts');
+
+  const handleSubmit = async () => {
+    const newPost = {
+      title,
+      content,
+      createdAt: new Date(),
+    }
+    try {
+      await addDoc(postsCollectionRef, newPost);
+      alert("投稿完了しました！")
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
-    <div>CreatePost</div>
+    < >
+    <form onSubmit={handleSubmit}>
+      <div>
+        <input
+          onChange={(e) => {setTitle(e.target.value)}} 
+          type="text"
+          value={title}/>
+      </div>
+      <div>
+        <textarea
+          onChange={(e) => {setContent(e.target.value)}} 
+          placeholder="本文を入力"
+          value={content}/>
+      </div>
+      <div>
+        <button type="submit">投稿</button>
+      </div>
+    </form>
+    </>
   )
 }
 

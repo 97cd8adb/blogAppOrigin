dc32df2c7afbe440844a4ccfee4add421e1e6452
1sh1d4 <isd97cd8adb@icloud.com>
2025-08-21 18:12:16 +0900

    HomePageコンポーネントに投稿の取得と削除機能を実装

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import { collection, deleteDoc, onSnapshot, doc } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
 const HomePage = () => {
+  const [posts, setPosts] = useState([]);
+  const db = getFirestore();
+  const postsCollectionRef = collection(db, 'posts');
+  const auth = getAuth();
+  const deletePost = async (postId) => {
+    const docRef = doc(db, "posts", postId);
+    await deleteDoc(docRef)
+  };
+  useEffect(() => {
+    const unsubscribe = onSnapshot(postsCollectionRef, (snapshot) => {
+      const postsData = snapshot.docs.map(doc => ({
+        id : doc.id,
+        ...doc.data()
+      }));
+      setPosts(postsData)
+    });
+    return () => unsubscribe();
+  }, []);
   return (
-    <div>HomePage</div>
+    <>
+    {posts.map(post => (
+      <div key={post.id}>
+        <h2>{post.title}</h2>
+        <p>{post.content}</p>
+        {post.uid === auth.currentUser ?.uid &&(
+          <button onClick={() => deletePost(post.id)}>削除</button>
+        )}
+      </div>
+    ))}
+    </>
   )
 }
 

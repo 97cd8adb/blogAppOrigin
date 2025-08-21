3dc2f9c90e9d0b4288e39f0411fd2c7f85f3f743
1sh1d4 <isd97cd8adb@icloud.com>
2025-08-21 19:35:01 +0900

    誤字修正

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -2,14 +2,14 @@ import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
-const Login = ({setIsLoggedin}) => {
+const Login = ({setIsLoggedIn}) => {
   const navigate = useNavigate();
   const handleLoginClick = async () => {
     const auth = getAuth();
     const provider = new GoogleAuthProvider();
       try {
         await signInWithPopup(auth, provider);
-        setIsLoggedin(true);
+        setIsLoggedIn(true);
         navigate("/");
       } catch (error) {
         console.error(error);
